03918c622445aaef1e3d14b315d5b2ffa4a7f5f1
nilton <nfescame@gmail.comq:qclear>
2022-08-20 00:20:33 -0300

    remove icon

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { Container, Content } from "./style";
-import {
-  FaLinkedin,
-  FaFacebook,
-  FaInstagram,
-  FaEnvelope,
-} from "react-icons/fa";
+import { FaLinkedin, FaFacebook, FaEnvelope } from "react-icons/fa";
 import ItemContact from "../../../components/ItemContact";
 
 export default function Contact() {
