22db48198b12276bf270909a9862bcf050afde55
nilton <nfescame@gmail.comq:qclear>
2022-08-20 00:18:45 -0300

    commit

---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container, Content } from "./style";
+import {
+  FaLinkedin,
+  FaFacebook,
+  FaInstagram,
+  FaEnvelope,
+} from "react-icons/fa";
+import ItemContact from "../../../components/ItemContact";
+
+export default function Contact() {
+  return (
+    <Container>
+      <Content>
+        <ItemContact
+          IconFa={FaLinkedin}
+          LinkContact='https://www.linkedin.com/in/nilton-escame/'
+        />
+        <ItemContact
+          IconFa={FaFacebook}
+          LinkContact='https://www.facebook.com/profile.php?id=100005693945352'
+        />
+
+        <ItemContact IconFa={FaEnvelope} LinkContact='nfescam@gmail.com' />
+      </Content>
+    </Container>
+  );
+}
